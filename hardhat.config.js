880cc815d0754b38a5f9276a0bc5680ddc85a852
naumanmehdi <100476953+naumanmehdi@users.noreply.github.com>
2022-08-14 18:44:04 +0500

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { MUMBAI_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.9",
-};
+   solidity: "0.8.4",
+   defaultNetwork: "mumbai",
+   networks: {
+      hardhat: {},
+      mumbai: {
+        url: MUMBAI_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
